7a889a433766e860d582dd95bff3b690ab04a396
jurekogorek <jerzy.czepiel@gmail.com>
2011-12-04 21:34:24 +0100

    projekt zliczacz

---
+++ zliczacz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zliczacz::Application.config.secret_token = 'ea277c0d794e61dda3fe911e823610c65a384be4b0f3d5444ded11ba854e5ff2921f1a2a033e701f97e10095a03838df73f428f00db937c6f8b43e98e50abe7b'
---
+++ zliczacz/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Zliczacz::Application.config.session_store :cookie_store, key: '_zliczacz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Zliczacz::Application.config.session_store :active_record_store
